24ceb5a1d17610a5ab0c4aec0392dd7832395f51
Phillip Lord <phillip.lord@newcastle.ac.uk>
2015-07-14 15:27:15 +0100

    Update to 1.4.0.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject lisbon "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Tutorial for Tawny-OWL"
+  :url "http://github.com/phillord/tawny-tutorial"
   :plugins [[lein-catnip "0.5.1" :exclusions [org.clojure/clojure]]]
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [uk.org.russet/tawny-owl "1.3.1-SNAPSHOT"]
+                 [uk.org.russet/tawny-owl "1.4.0"]
                  [sio-maven "1.0.0"]]
   :main lisbon.core)
