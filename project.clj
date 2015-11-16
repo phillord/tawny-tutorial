a64bd818aacd6495b75f369ab8e5ed01928e3629
Jennifer Warrender <jaydchan@gmail.com>
2015-11-16 10:00:50 +0000

    removed catnip plugin

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject tawny.tutorial "1.0"
   :description "A Tutorial for Tawny-OWL"
   :url "http://github.com/phillord/tawny-tutorial"
-  :plugins [[lein-catnip "0.5.1" :exclusions [org.clojure/clojure]]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [uk.org.russet/tawny-owl "1.4.0"]
                  [sio-maven "1.0.0"]]
